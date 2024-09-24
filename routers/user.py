0e343dce1cc1e22a0336d67a1c231c6db0fb187d
Harish Soren <48867706+Harry-s10@users.noreply.github.com>
2024-09-24 18:53:55 +0530

    Added authentication.py and removed oauth2.py and token.py in order to fix authentication.

--- routers/user.py
+++ routers/user.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from .. import oauth2
-from ..app_db import database, schemas, user_crud
+from app_db import database, schemas, user_crud
+from authentication import get_current_active_user
 
 router = APIRouter(
         prefix="/user",
@@ -12,7 +12,7 @@ router = APIRouter(
 
 @router.post("/")
 def create_user(request: schemas.UserCreate, db: Session = Depends(database.get_db),
-                current_user: schemas.User = Depends(oauth2.get_current_user)):
+                current_user: schemas.User = Depends(get_current_active_user)):
     """Create new user"""
     return user_crud.create(request, db)
 
@@ -25,6 +25,6 @@ def get_user(id: int, db: Session = Depends(database.get_db)):
 
 @router.get("/{id}/blogs", response_model=schemas.UserBlogs)
 def get_user_blogs(id: int, db: Session = Depends(database.get_db),
-                   current_user: schemas.User = Depends(oauth2.get_current_user)):
+                   current_user: schemas.User = Depends(get_current_active_user)):
     """Get all user's blogs based on ID"""
     return user_crud.get_user(id, db)
